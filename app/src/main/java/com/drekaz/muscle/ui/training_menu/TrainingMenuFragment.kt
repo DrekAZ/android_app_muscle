a6504ec4762a1fefe80ec515596a16329061bb52
A <kassuruv2@gmail.com>
2020-11-04 00:46:12 +0900

    add gif image for training

--- app/src/main/java/com/drekaz/muscle/ui/training_menu/TrainingMenuFragment.kt
+++ app/src/main/java/com/drekaz/muscle/ui/training_menu/TrainingMenuFragment.kt
@@ -1,8 +1,9 @@
 package com.drekaz.muscle.ui.training_menu
 
+import android.app.Activity
 import android.app.AlertDialog
-import android.app.Dialog
 import android.content.DialogInterface
+import android.media.Image
 import android.os.Bundle
 import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
@@ -13,13 +14,11 @@ import android.widget.ImageView
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+
 import com.drekaz.muscle.R
 
 class TrainingMenuFragment : Fragment() {
-    companion object {
-        const val TAG = "TrainingMenuFragment"
-    }
-
     private val menus = arrayOf(
         "腕立て伏せ",
         "プランク",
@@ -41,15 +40,21 @@ class TrainingMenuFragment : Fragment() {
         val listView : ListView = view.findViewById(R.id.list_view)
         val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, menus)
         listView.adapter = adapter
-        listView.setOnItemClickListener { parent, view, position, id ->
+        listView.setOnItemClickListener { parent, view_list, position, id ->
             val element = parent.getItemAtPosition(position)
-            val builder = AlertDialog.Builder(ContextThemeWrapper(view.context, R.style.Theme_AppCompat_Light_Dialog)).apply {
+            val gifView = ImageView(view.context)
+            //val imageViewTarget = GlideDrawableImageViewTarget(gifView)
+            val builder = AlertDialog.Builder(view.context).apply {
                 setTitle(element.toString())
+                setMessage("HALLO")
                 setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
                     findNavController().navigate(R.id.action_menu_training)
                 })
                 setNegativeButton("cancel", DialogInterface.OnClickListener {dialog, which ->
                 })
+                setView(gifView)
+                //Glide.with(view.context).load(R.raw.pushup).into(gifView)
+                Glide.with(context).load(R.raw.pushup).override(480,270).into(gifView)
             }
             builder.show()
 
