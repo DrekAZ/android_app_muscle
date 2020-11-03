a8b438a6f2471738af01337e450e71f56c89e2b9
A <kassuruv2@gmail.com>
2020-11-03 16:48:38 +0900

    add Glide library and tutorial-gif-dialog feature

--- app/src/main/java/com/drekaz/muscle/ui/training_menu/TrainingMenuFragment.kt
+++ app/src/main/java/com/drekaz/muscle/ui/training_menu/TrainingMenuFragment.kt
@@ -1,12 +1,18 @@
 package com.drekaz.muscle.ui.training_menu
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.drekaz.muscle.R
 
 class TrainingMenuFragment : Fragment() {
@@ -14,7 +20,7 @@ class TrainingMenuFragment : Fragment() {
         const val TAG = "TrainingMenuFragment"
     }
 
-    private val menus = arrayOf<String>(
+    private val menus = arrayOf(
         "腕立て伏せ",
         "プランク",
         "クランチ",
@@ -32,8 +38,21 @@ class TrainingMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val listView : ListView = view.findViewById<ListView>(R.id.list_view)
-        val adapter = ArrayAdapter<String>(view.context, android.R.layout.simple_list_item_1, menus)
+        val listView : ListView = view.findViewById(R.id.list_view)
+        val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, menus)
         listView.adapter = adapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val element = parent.getItemAtPosition(position)
+            val builder = AlertDialog.Builder(ContextThemeWrapper(view.context, R.style.Theme_AppCompat_Light_Dialog)).apply {
+                setTitle(element.toString())
+                setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                    findNavController().navigate(R.id.action_menu_training)
+                })
+                setNegativeButton("cancel", DialogInterface.OnClickListener {dialog, which ->
+                })
+            }
+            builder.show()
+
+        }
     }
 }
