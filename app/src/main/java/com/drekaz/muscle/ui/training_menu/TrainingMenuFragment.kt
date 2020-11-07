bf4ee905bef77bcb26699ea3d1f4b3cfcdd02ed9
A <kassuruv2@gmail.com>
2020-11-07 11:00:43 +0900

    rewrite

--- app/src/main/java/com/drekaz/muscle/ui/training_menu/TrainingMenuFragment.kt
+++ app/src/main/java/com/drekaz/muscle/ui/training_menu/TrainingMenuFragment.kt
@@ -17,14 +17,9 @@ import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 
 import com.drekaz.muscle.R
+import com.drekaz.muscle.ui.dialog.DialogTrainingMenu
 
 class TrainingMenuFragment : Fragment() {
-    private val menus = arrayOf(
-        "腕立て伏せ",
-        "プランク",
-        "クランチ",
-        "スクワット",
-    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,27 +32,14 @@ class TrainingMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val menus = resources.getStringArray(R.array.menus)
         val listView : ListView = view.findViewById(R.id.list_view)
         val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, menus)
         listView.adapter = adapter
         listView.setOnItemClickListener { parent, view_list, position, id ->
             val element = parent.getItemAtPosition(position)
-            val gifView = ImageView(view.context)
-            //val imageViewTarget = GlideDrawableImageViewTarget(gifView)
-            val builder = AlertDialog.Builder(view.context).apply {
-                setTitle(element.toString())
-                setMessage("HALLO")
-                setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
-                    findNavController().navigate(R.id.action_menu_training)
-                })
-                setNegativeButton("cancel", DialogInterface.OnClickListener {dialog, which ->
-                })
-                setView(gifView)
-                //Glide.with(view.context).load(R.raw.pushup).into(gifView)
-                Glide.with(context).load(R.raw.pushup).override(480,270).into(gifView)
-            }
-            builder.show()
-
+            val dialog = DialogTrainingMenu(element.toString())
+            dialog.show(parentFragmentManager, null)
         }
     }
 }
