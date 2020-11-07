8154a9f56e27c8eb34ce23ed2bca715a06ed43d6
A <kassuruv2@gmail.com>
2020-11-07 10:56:51 +0900

    fragment Fullscreen

--- app/src/main/java/com/drekaz/muscle/MainActivity.kt
+++ app/src/main/java/com/drekaz/muscle/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.drekaz.muscle
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,7 +22,16 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_setting))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        setFullscreen(navController, navView)
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+
+    private fun setFullscreen(navController: NavController, navView: BottomNavigationView) {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if(destination.id == R.id.fragment_training) supportActionBar?.hide()
+            navView.visibility = if(destination.id == R.id.fragment_training) View.GONE else View.VISIBLE
+        }
+    }
 }
